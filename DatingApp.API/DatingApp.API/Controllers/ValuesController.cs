b7bbaf2e419d3954227030ffe17a71009cff1e91
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-07-20 21:00:30 -0300

    Start DatingApp project

---
+++ DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValue()
+        {
+            var values = await _context.Values.ToListAsync();
+           
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var valor = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if (valor == null)
+            {
+                return NotFound("Valor não encontrado");
+            }
+
+            return Ok(valor);
+        }
+    }
+}
